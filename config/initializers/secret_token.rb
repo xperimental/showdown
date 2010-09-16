ea0f29210fe5342361ae1aed7166c0efe5587ff1
Robert <xperimental@solidproject.de>
2010-09-16 15:26:09 +0200

    Created rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showdown::Application.config.secret_token = 'f5035e756f070b2f334ca5e343fc928816b0eb14a3dff49444608690d9df33c28e335cfafa5a50ef1637c5aa0b33a49efa09fb09f97f59e8bdec24707ef8c25f'
